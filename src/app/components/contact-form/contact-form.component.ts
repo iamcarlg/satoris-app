edda7983716e55ff23bf7dd1711ee14bb2d69032
Carl Gauss Rugero <rugerocarlgauss@gmail.com>
2025-03-25 17:52:12 +0100

    Fix du bug sur gb en déclarant contactForm comme propriété de type FormGroup et Initialisation du formulaire dans le constructeur

--- src/app/components/contact-form/contact-form.component.ts
+++ src/app/components/contact-form/contact-form.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule ,Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule ,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
@@ -10,21 +10,21 @@ import { FormBuilder, ReactiveFormsModule ,Validators } from '@angular/forms';
   standalone: true,
 })
 export class ContactFormComponent {
+  contactForm: FormGroup;
+  msgSsent = false;
 
-  constructor (private fb: FormBuilder) {};
-
-  // The form group for the contact form
-  contactForm = this.fb.group({
-    firstName: ['', Validators.required],
-    lastName: ['', Validators.required],
-    email: [''],
-    address: ['']
-  })
-  
-  sent = false;
+  // The contructor is called when the component is created
+  constructor (private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: [''],
+      address: ['']
+    })
+  };  
 
   onSubmit() {
     console.log('Form successfully submitted!');
-    this.sent = true;
+    this.msgSsent = true;
   }
 }
