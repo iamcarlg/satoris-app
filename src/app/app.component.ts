0ab7326c9d18d7d42494bde75d99105ec8d00889
Carl Gauss Rugero <rugerocarlgauss@gmail.com>
2025-03-25 13:21:38 +0100

    Ajout du module Google Maps et intégration d'une carte dans le composant principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileCardComponent } from './components/profile-card/profile-card.component';
+import { GoogleMapsModule } from '@angular/google-maps'
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports : [CommonModule, ProfileCardComponent],
+  imports : [CommonModule, ProfileCardComponent, GoogleMapsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'satoris-app';
+  center: google.maps.LatLngLiteral = {lat: 24, lng: 12};
 }
